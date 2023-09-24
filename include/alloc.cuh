0fa1211c118b5d4f478c8db0007aaf4acb468f04
Bogdan Morosanu <bogdan.moro.mail@gmail.com>
2023-09-24 16:38:26 +0200

    Make ImageBuffer cross-host-device compatible

--- include/alloc.cuh
+++ include/alloc.cuh
@@ -2,37 +2,105 @@
 
 #include <cstdint>
 
+#include <Eigen/Dense>
+
 #include "check_error.cuh"
 
 namespace rt {
+  class ImageBufferView;
+
   class ImageBuffer {
   public:
-    ImageBuffer(std::size_t w, std::size_t h)
-      : width_(w)
-      , height_(h)
-      , alloc_size_(w * h * 3 * sizeof(float))
-      , buffer_(nullptr)
+
+    friend class ImageBufferView;
+    
+    using ValueType = Eigen::Vector3f;
+    
+    __host__ ImageBuffer(std::size_t w, std::size_t h)
+      : header_(nullptr)
+      , buffer_(nullptr)	
     {
-      checkCudaErrors(cudaMallocManaged((void **)&buffer_, alloc_size_));
+      // TODO checkout best buffer alignment for runtime perf
+      // pad pixel data to be aligned 64 bytes
+      auto align = 64;
+      auto padding = align - 1;
+      auto alloc_size = (sizeof(Header) + padding + w * h * sizeof(ValueType));
+
+      checkCudaErrors(cudaMallocManaged((void **)&header_, alloc_size));
+      new (header_) Header{w, h, alloc_size};
+
+      auto align_offset = align - reinterpret_cast<std::uintptr_t>(header_) % align;
+      buffer_ = reinterpret_cast<ValueType*>(
+                  reinterpret_cast<char*>(header_) + sizeof(Header) + align_offset
+		);
     }
 
-    float *buffer() { return buffer_; }
+    __host__ ImageBuffer(const ImageBuffer&) = delete;
+    __host__ ImageBuffer& operator = (const ImageBuffer&) = delete;
 
-    const float *buffer() const  { return buffer_; }
-
-    std::size_t width() const { return width_; }
-
-    std::size_t height() const { return height_; }
-    
-    ~ImageBuffer()
+    __host__ ImageBuffer(ImageBuffer &&other)
+      : header_(other.header_)
+      , buffer_(other.buffer_)
     {
-      checkCudaErrors(cudaFree(buffer_));
+      other.header_ = nullptr;
+      other.buffer_ = nullptr;
+    }
+
+    __host__ ImageBuffer &operator = (ImageBuffer &&other)
+    {
+      // guard against this = std::move(*this) blowing our object up
+      if (this != &other) {
+	this->~ImageBuffer();
+	new (this) ImageBuffer(std::move(other));
+      }
+
+      return *this;
+    }
+
+    __host__ __device__ ValueType *buffer() { return buffer_; }
+
+    __host__ __device__ const ValueType *buffer() const  { return buffer_; }
+
+    __host__ __device__ std::size_t width() const { return header_->width_; }
+
+    __host__ __device__ std::size_t height() const { return header_->height_; }
+
+    __host__ ~ImageBuffer()
+    {
+      if (header_) {
+	checkCudaErrors(cudaFree(header_));
+      }
     }
     
   private:
-    std::size_t width_;
-    std::size_t height_;
-    std::size_t alloc_size_;
-    float *buffer_;
+
+    struct Header {
+      std::size_t width_;
+      std::size_t height_;
+      std::size_t alloc_size_;
+    };
+
+    Header *header_;
+    ValueType *buffer_;
+  };
+
+  class ImageBufferView {
+  public:
+    __host__ ImageBufferView(ImageBuffer &data)
+      : header_(data.header_)
+      , buffer_(data.buffer_)
+    { }
+
+    __host__ __device__ ImageBuffer::ValueType *buffer() { return buffer_; }
+
+    __host__ __device__ const ImageBuffer::ValueType *buffer() const  { return buffer_; }
+
+    __host__ __device__ std::size_t width() const { return header_->width_; }
+
+    __host__ __device__ std::size_t height() const { return header_->height_; }
+    
+  private:
+    ImageBuffer::Header *header_;
+    ImageBuffer::ValueType *buffer_;
   };
 }
--- src/main.cu
+++ src/main.cu
@@ -15,16 +15,16 @@ void print_help_and_exit(const char *name) {
   std::exit(EXIT_FAILURE);
 }
 
-__global__ void render(float *buff, int max_row, int max_col)
+__global__ void render(rt::ImageBufferView img)
 {
   int row = threadIdx.x + blockIdx.x * blockDim.x;
   int col = threadIdx.y + blockIdx.y * blockDim.y;
 
-  if (row < max_row && col < max_col) {
-    int pixel_index = (col * max_row + row) * 3;
-    buff[pixel_index] = float(row) / max_row;
-    buff[pixel_index + 1] = float(col) / max_col;
-    buff[pixel_index + 2] = 0.2f;
+  if (row < img.width() && col < img.height()) {
+    int pixel_index = col * img.width() + row;
+    img.buffer()[pixel_index][0] = float(row) / img.width();
+    img.buffer()[pixel_index][1] = float(col) / img.height();
+    img.buffer()[pixel_index][2] = 0.2f;
   }
 }
 
@@ -34,7 +34,7 @@ int main(int argc, char **argv)
     print_help_and_exit(*argv);
   }
   
-  rt::ImageBuffer img(256, 128);
+  rt::ImageBuffer img(2048, 1024);
 
   int threads_w = 8;
   int threads_h = 8;
@@ -48,12 +48,12 @@ int main(int argc, char **argv)
 
   std::printf("running (%lu, %lu) blocks\n", w_partition.elements_per_thread, h_partition.elements_per_thread);
   
-  render<<<blocks, threads>>>(img.buffer(), img.width(), img.height());
+  render<<<blocks, threads>>>(rt::ImageBufferView(img));
 
   checkCudaErrors(cudaGetLastError());
   checkCudaErrors(cudaDeviceSynchronize());
 
-  stbi_write_hdr(argv[1], img.width(), img.height(), 3, img.buffer());
+  stbi_write_hdr(argv[1], img.width(), img.height(), 3, &(img.buffer()[0][0]));
   
   std::printf("Hello, world!\n");
   return 0;
